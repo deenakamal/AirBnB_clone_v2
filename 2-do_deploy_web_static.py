58e4f602ca5c08e9845aee56273a1f8036a91ad9
deenakamal <deenah.kamal@gmail.com>
2024-04-07 12:42:36 -0700

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
 """Deploy archive!"""
 
-from fabric.contrib import files
-from fabric.api import *
-import os
+from fabric.api import put, run, env
+from os.path import exists
 
 env.user = "ubuntu"
 env.hosts = ["54.197.110.17", "54.237.32.90"]
 
 
 def do_deploy(archive_path):
-    """Function for deploy"""
-    if not os.path.exists(archive_path):
+    """distribute archive
+    to web servers
+    """
+    if exists(archive_path) is False:
         return False
-
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
-
     try:
-        put(archive_path, '/tmp')
-        run('sudo mkdir -p {}'.format(dest))
-        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('sudo rm -f /tmp/{}.tgz'.format(name))
-        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
-        run('sudo rm -rf {}/web_static'.format(dest))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -sf {} /data/web_static/current'.format(dest))
-        run('sudo chown -R ubuntu:ubuntu /data/')
+        file_name = archive_path.split('/')[-1]
+        no_exten = file_name.split('.')[0]
+        path = '/data/web_static/releases/'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_exten))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_exten))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_exten))
+        run('rm -rf {}{}/web_static'.format(path, no_exten))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_exten))
         return True
-    except:
+    except Exception:
         return False
