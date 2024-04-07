59117b812fd455d63b2d4f1931517dc26878df7f
deenakamal <deenah.kamal@gmail.com>
2024-04-07 14:25:43 -0700

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """Distrubute archive"""
+
 from fabric.api import *
 import os
-
+from fabric.contrib import files
 
 env.user = 'ubuntu'
 env.hosts = ['54.197.110.17', '54.237.32.90']
 
 
 def do_deploy(archive_path):
-    """do_deploy to distribute archive to web servers"""
+    """do_deploy"""
     if not os.path.exists(archive_path):
         return False
 
+    _path = '/data/web_static/releases/'
+    name = archive_path.split('/')[-1].split('.')[0]
+    last = _path + name
     try:
-        put("{:s}".format(archive_path), "/tmp/")
-        new_dir = "/data/web_static/releases/" + \
-                          os.path.basename(archive_path).split('.')[0] + '/'
-        run('mkdir -p {:s}'.format(new_release_dir))
-        archived_file = os.path.basename(archive_path)
-        run('sudo tar -xzf /tmp/{:s} -C {:s}'
-            .format(archived_file, new_dir))
-        run('sudo mv {:s}web_static/* {:s}'
-            .format(new_dir, new_dir))
-        run('sudo rm -rf /tmp/{:s}'.format(archived_file))
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, last))
+        run('sudo rm -f /tmp/{}.tgz'.format(name))
+        run('sudo mv {}/web_static/* {}/'.format(last, last))
+        run('sudo rm -rf {}/web_static'.format(last))
         run('sudo rm -rf /data/web_static/current')
-        run('ln -sf {:s} /data/web_static/current'.format(new_dir))
+        run('sudo ln -sf {} /data/web_static/current'.format(last))
+        run('sudo chown -R ubuntu:ubuntu /data/')
         return True
-
     except Exception:
         return False
