6b5b648294a588de664e9908e227a218d9e0c180
deenakamal <deenah.kamal@gmail.com>
2024-04-07 11:08:44 -0700

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""generates a .tgz archive
-from web_static folder
-"""
+"""Generates a .tgz archive."""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """generates archive from folder"""
+    """generates archive"""
+    file_date = datetime.now().strftime('%Y%m%d%H%M%S')
+    file_name = f'web_static_{fdate}.tgz'
     try:
-        fdate = datetime.now().strftime('%Y%m%d%H%M%S')
-        fileN = f'web_static_{fdate}.tgz'
         local('mkdir -p versions')
-        local(f'tar -cvzf versions/{fileN} web_static')
-        return f'versions/{fileN}'
+        local(f'tar -cvzf versions/{file_name} web_static')
     except Exception:
         return None
+    return f'versions/{file_name}'
