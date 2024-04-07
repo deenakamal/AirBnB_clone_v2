187b314e40e0a08ee45bbd3bf45ea0ac6046b87b
deenakamal <deenah.kamal@gmail.com>
2024-04-07 12:01:23 -0700

    f

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,39 +1,32 @@
 #!/usr/bin/python3
 """Deploy archive!"""
 
-from fabric.api import *
+from fabric.contrib import files
+from fabric.api import env, put, run
 import os
 
 env.user = "ubuntu"
 env.hosts = ["54.197.110.17", "54.237.32.90"]
 
-
 def do_deploy(archive_path):
-    """distributes an archive to a web servers"""
+    """Function for deploy"""
     if not os.path.exists(archive_path):
         return False
 
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
     try:
-        put("{:s}".format(archive_path), "/tmp/")
-
-        new_release_dir = "/data/web_static/releases/" + \
-                          os.path.basename(archive_path).split('.')[0] + '/'
-        run('mkdir -p {:s}'.format(new_release_dir))
-
-        archived_file = os.path.basename(archive_path)
-        run('sudo tar -xzf /tmp/{:s} -C {:s}'
-            .format(archived_file, new_release_dir))
-
-        run('sudo mv {:s}web_static/* {:s}'
-            .format(new_release_dir, new_release_dir))
-
-        run('sudo rm -rf /tmp/{:s}'.format(archived_file))
-
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('sudo rm -f /tmp/{}.tgz'.format(name))
+        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
+        run('sudo rm -rf {}/web_static'.format(dest))
         run('sudo rm -rf /data/web_static/current')
-
-        run('ln -sf {:s} /data/web_static/current'.format(new_release_dir))
-
+        run('sudo ln -s {} /data/web_static/current'.format(dest))
         return True
-
-    except Exception:
+    except:
         return False
