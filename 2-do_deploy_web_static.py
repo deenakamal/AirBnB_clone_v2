8f0a0c78191f3ae1b1f9a781e90b3492b424d2bb
deenakamal <deenah.kamal@gmail.com>
2024-04-07 14:03:46 -0700

    task 222

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-"""Deploy archive!"""
-
+"""Distrubute archive"""
 from fabric.api import *
 import os
-from fabric.contrib.files import exists
+
 
 env.user = 'ubuntu'
 env.hosts = ['54.197.110.17', '54.237.32.90']
-env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    if not exists(archive_path):
+    """do_deploy to distribute archive to web servers"""
+    if not os.path.exists(archive_path):
         return False
 
     try:
-        filename = archive_path.split('/')[-1]
-        no_ext = filename.split('.')[0]
-        release_path = f'/data/web_static/releases/{no_ext}/'
-        tmp_path = '/tmp/' + filename
-
-        put(archive_path, tmp_path)
-        run(f'mkdir -p {release_path}')
-        run(f'tar -xzf {tmp_path} -C {release_path}')
-        run(f'rm {tmp_path}')
-        run(f'mv {release_path}web_static/* {release_path}')
-        run(f'rm -rf {release_path}web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {release_path} /data/web_static/current')
-
-        print("New version deployed!")
+        put("{:s}".format(archive_path), "/tmp/")
+        new_dir = "/data/web_static/releases/" + \
+                          os.path.basename(archive_path).split('.')[0] + '/'
+        run('mkdir -p {:s}'.format(new_release_dir))
+        archived_file = os.path.basename(archive_path)
+        run('sudo tar -xzf /tmp/{:s} -C {:s}'
+            .format(archived_file, new_dir))
+        run('sudo mv {:s}web_static/* {:s}'
+            .format(new_dir, new_dir))
+        run('sudo rm -rf /tmp/{:s}'.format(archived_file))
+        run('sudo rm -rf /data/web_static/current')
+        run('ln -sf {:s} /data/web_static/current'.format(new_dir))
         return True
-    except:
+
+    except Exception:
         return False
