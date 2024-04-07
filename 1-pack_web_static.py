32307ec68d9bc6f6624c3ec03256c1d0713ae099
deenakamal <deenah.kamal@gmail.com>
2024-04-07 11:10:44 -0700

    edit pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,14 +3,14 @@
 from fabric.api import local
 from datetime import datetime
 
-
 def do_pack():
     """generates archive"""
     file_date = datetime.now().strftime('%Y%m%d%H%M%S')
-    file_name = f'web_static_{fdate}.tgz'
+    file_name = f'web_static_{file_date}.tgz'
     try:
         local('mkdir -p versions')
-        local(f'tar -cvzf versions/{file_name} web_static')
-    except Exception:
+        local(f'tar -czvf versions/{file_name} web_static')
+        return f'versions/{file_name}'
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
-    return f'versions/{file_name}'
