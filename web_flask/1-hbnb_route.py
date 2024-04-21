09f54c87b139a55d5dbcabe2057839f200781d67
deenakamal <deenah.kamal@gmail.com>
2024-04-21 14:01:52 -0700

    :)

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Flask web application.
+"""""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """a function that diplays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ a function that displays HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """Listening on"""
+    app.run(port=5000, host='0.0.0.0')
