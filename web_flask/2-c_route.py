ba0f243f3b7a42d84aa02903c2a7e3ea3d2c5928
deenakamal <deenah.kamal@gmail.com>
2024-04-21 14:06:07 -0700

    3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """a function thay displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """a function that displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """with variable"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
